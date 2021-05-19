2f9d16932c139981846bd04ae5de4b8bea5975b7
jinwoo park <swat018@gmail.com>
2021-05-19 10:51:14 +0900

    프로덕션 코드(Calculator 클래스)와 테스트 코드(CalculatorTest)를 분리

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,9 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(3,4));
+        System.out.println(cal.subtract(5,4));
+        System.out.println(cal.multiply(2,6));
+        System.out.println(cal.divide(8,4));
+    }
+}
