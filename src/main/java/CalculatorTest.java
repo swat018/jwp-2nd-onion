540379fb73276ac5fb0d6ded24082d382dbe79bb
jinwoo park <swat018@gmail.com>
2021-05-19 11:01:54 +0900

    테스트 코드를 각 메소드별로 분리

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -1,9 +1,24 @@
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(3,4));
-        System.out.println(cal.subtract(5,4));
-        System.out.println(cal.multiply(2,6));
-        System.out.println(cal.divide(8,4));
+        add(cal);
+        subtract(cal);
+        multiply(cal);
+        divide(cal);
+    }
+    private static void divide(Calculator cal) {
+        System.out.println(cal.divide(9,3));
+    }
+
+    private static void multiply(Calculator cal) {
+        System.out.println(cal.multiply(9,3));
+    }
+
+    private static void subtract(Calculator cal) {
+        System.out.println(cal.subtract(9,3));
+    }
+
+    private static void add(Calculator cal) {
+        System.out.println(cal.add(9,3));
     }
 }
