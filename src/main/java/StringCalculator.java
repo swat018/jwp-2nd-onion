c7838daff9a30fe94f7956c5b5e6d16c2d9c2471
jinwoo park <swat018@gmail.com>
2021-05-22 16:44:21 +0900

    Chapter2. 문자열 계산기 구현 및 리펙토리 3단계

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     int add(String text) {
         if (isBlank(text)) {
@@ -11,16 +14,31 @@ public class StringCalculator {
     }
 
     private String[] split(String text) {
-        String[] values  = text.split(",");
-        return values;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String cumstomDelimeter = m.group(1);
+            return m.group(2).split(cumstomDelimeter);
+        }
+
+        return text.split(",|;");
     }
 
     private int[] toInts(String[] values) {
         int[] numbers = new int[values.length];
-        for(int i=0; i<values.length;i++) {
-            numbers[i] = Integer.parseInt(values[i]);
+        for (int i=0; i<values.length;i++) {
+            int number = toPositive(values[i]);
+            numbers[i] = number;
         }
         return numbers;
+    }
+
+    private int toPositive(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+
     }
 
     private int sum(int[] numbers) {
